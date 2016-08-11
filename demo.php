08c5e94690f34199a59b920a771f6d3e48c0f8fb
dimsaf <dimsaf@mail.ru>
2016-08-11 21:52:32 +0700

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,6 +1,4 @@
 <?php
-// you can write to stdout for debugging purposes, e.g.
-// print "this is a debug message\n";
 /**
  * 
  * this solution get 100% on Codility.com!
@@ -8,9 +6,6 @@
  * @author saf
  *
  */
-
-$A=[-1, 3, -4, 5, 1, -6, 2, 1];
-
 function solution($A) {
     if ((count($A)==0)||(count($A)>100000)) return -1;
     $leftSum=$rightSum=0;
@@ -23,5 +18,5 @@ function solution($A) {
     }
     return -1;
 }
-echo solution($A);
+echo solution([-1, 3, -4, 5, 1, -6, 2, 1]);
 ?>

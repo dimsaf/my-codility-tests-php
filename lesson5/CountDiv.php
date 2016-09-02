d05b7b600ceaf9b6152d5ef8308bb8bdba013ccd
dimsaf <dimsaf@mail.ru>
2016-09-02 22:29:19 +0700

    Update CountDiv.php

--- lesson5/CountDiv.php
+++ lesson5/CountDiv.php
@@ -9,10 +9,7 @@
 
 function solution($A, $B, $K) {
     $inc=0;
-    if ($A>$B) return -1;
-    if ($A < 0 || $A > 2000000000) return -1;
-    if ($B < 0 || $B > 2000000000) return -1;
-    if ($K < 1 || $K > 2000000000) return -1;
+    if (($A>$B) || ($A < 0 || $A > 2000000000) || ($B < 0 || $B > 2000000000) || ($K < 1 || $K > 2000000000)) return -1;
     if ($B===0) return 1;
     if ($K>$B) return 0;
     if (($A%$K===0)||($B%$K===0)||($K>$A)) $inc=1; 
@@ -20,5 +17,5 @@ function solution($A, $B, $K) {
     return (int)floor(($B-max($A,$K))/$K)+$inc;
 }
 
-echo solution(11,11,2);
+echo solution(6,11,2);
 ?>
